d02f2abb178db14540a7ef43a5006f2fa0917e1f
Nick Kasten <kastentx@gmail.com>
2018-11-06 10:15:40 -0600

    adding CORS support

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
+import logging
 from flask import Flask
 from api import api
+from flask_cors import CORS
 
 app = Flask(__name__)
 # load default config
@@ -9,6 +11,12 @@ app.config.from_object('config')
 if 'APP_CONFIG' in os.environ:
     app.config.from_envvar('APP_CONFIG')
 api.init_app(app)
+# enable CORS if flag is set
+if os.getenv('CORS_ENABLE') == 'true':
+    CORS(app, origins='*')
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('MAX Model Server is currently ' + \
+    'allowing cross-origin requests - (CORS ENABLED)')    
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
