128d69d9956abb3cbd59e941887b2dbf6fc9bca9
Nick Pentreath <nickp@za.ibm.com>
2019-03-05 10:34:11 +0200

    Bump max-base to v1.1.0, updates for new maxfw

--- config.py
+++ config.py
@@ -5,6 +5,10 @@ DEBUG = False
 RESTPLUS_MASK_SWAGGER = False
 
 # Application settings
+API_TITLE = 'MAX Named Entity Tagger'
+API_DESC = 'Locate and tag named entities in text'
+API_VERSION = '1.1.0'
+
 MODEL_NAME = 'Named Entity Recognition'
 MODEL_ID = 'ner_model'
 DEFAULT_MODEL_PATH = 'assets'
@@ -15,5 +19,6 @@ MODEL_META_DATA = {
     'name': '{}'.format(MODEL_NAME),
     'description': '{} model trained on a subset of the Groningen Meaning Bank (GMB) dataset'.format(MODEL_NAME),
     'type': 'Natural Language Processing',
-    'license': '{}'.format(MODEL_LICENSE)
+    'license': '{}'.format(MODEL_LICENSE),
+    'source': 'https://developer.ibm.com/exchanges/models/all/max-named-entity-tagger/'
 }
