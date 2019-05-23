261d82228269dfb79938d80102ad44ae2a5dade8
Nick Pentreath <nickp@za.ibm.com>
2019-05-23 10:40:30 +0200

    Fix MyIOError and other flake8 issues. Fixes #13

--- core/utils.py
+++ core/utils.py
@@ -3,6 +3,7 @@
 UNK = "$UNK$"
 NUM = "$NUM$"
 
+
 def load_vocab(filename):
     """Loads vocab from a file
     Args:
@@ -10,16 +11,13 @@ def load_vocab(filename):
     Returns:
         d: dict[word] = index
     """
-    try:
-        d = dict()
-        with open(filename, encoding='ISO-8859-1') as f:
-            for idx, word in enumerate(f):
-                word = word.strip()
-                d[word] = idx
-
-    except IOError:
-        raise MyIOError(filename)
+    d = dict()
+    with open(filename, encoding='ISO-8859-1') as f:
+        for idx, word in enumerate(f):
+            word = word.strip()
+            d[word] = idx
     return d
+
 
 def get_processing_word(vocab_words=None, vocab_chars=None, lowercase=False, chars=False, allow_unk=True):
     """Return lambda function that transform a word (string) into list,
@@ -33,7 +31,7 @@ def get_processing_word(vocab_words=None, vocab_chars=None, lowercase=False, cha
     """
     def f(word):
         # 0. get chars of words
-        if vocab_chars is not None and chars == True:
+        if vocab_chars is not None and chars is True:
             char_ids = []
             for char in word:
                 # ignore chars out of vocabulary
@@ -54,11 +52,11 @@ def get_processing_word(vocab_words=None, vocab_chars=None, lowercase=False, cha
                 if allow_unk:
                     word = vocab_words[UNK]
                 else:
-                    raise Exception("Unknow key is not allowed. Check that "\
+                    raise Exception("Unknow key is not allowed. Check that "
                                     "your vocab (tags?) is correct")
 
         # 3. return tuple char ids, word id
-        if vocab_chars is not None and chars == True:
+        if vocab_chars is not None and chars is True:
             return char_ids, word
         else:
             return word
@@ -79,7 +77,7 @@ def _pad_sequences(sequences, pad_tok, max_length):
     for seq in sequences:
         seq = list(seq)
         seq_ = seq[:max_length] + [pad_tok]*max(max_length - len(seq), 0)
-        sequence_padded +=  [seq_]
+        sequence_padded += [seq_]
         sequence_length += [min(len(seq), max_length)]
 
     return sequence_padded, sequence_length
@@ -95,9 +93,9 @@ def pad_sequences(sequences, pad_tok, nlevels=1):
         a list of list where each sublist has same length
     """
     if nlevels == 1:
-        max_length = max(map(lambda x : len(x), sequences))
+        max_length = max(map(lambda x: len(x), sequences))
         sequence_padded, sequence_length = _pad_sequences(sequences,
-                                            pad_tok, max_length)
+                                                          pad_tok, max_length)
 
     elif nlevels == 2:
         max_length_word = max([max(map(lambda x: len(x), seq))
@@ -109,10 +107,10 @@ def pad_sequences(sequences, pad_tok, nlevels=1):
             sequence_padded += [sp]
             sequence_length += [sl]
 
-        max_length_sentence = max(map(lambda x : len(x), sequences))
+        max_length_sentence = max(map(lambda x: len(x), sequences))
         sequence_padded, _ = _pad_sequences(sequence_padded,
-                [pad_tok]*max_length_word, max_length_sentence)
+                                            [pad_tok]*max_length_word, max_length_sentence)
         sequence_length, _ = _pad_sequences(sequence_length, 0,
-                max_length_sentence)
+                                            max_length_sentence)
 
     return sequence_padded, sequence_length
